4c4f1f35f242be51ec95dc8bd5ecb38da0708e09
Sourabh9879 <kumarsourabh970@gmail.com>
2025-09-13 19:26:11 +0530

    insertion sort

---
+++ practice/insertionsort.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main(){
+    
+    int arr[] = {2,3,5,4,1,6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    for(int i = 1;i<n;i++){
+        int j = i;
+        while(j > 0 && arr[j-1] > arr[j]){
+            int key = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = key;
+            j--;
+        }
+    }
+    
+    for(int k =0;k<n;k++){
+        printf("%d",arr[k]);
+    }
+}
